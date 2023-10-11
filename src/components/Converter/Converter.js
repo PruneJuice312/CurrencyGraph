b6384424e123b529cc50f1b74d5425278e9b2047
acjenkins6440 <acjenkins6440@gmail.com>
2023-10-11 14:27:14 -0400

    Adding notes

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -5,6 +5,8 @@ const Converter = () => {
   const [currencyList, setCurrencyList] = useState(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  // I added this piece of state to hold the results of the conversion request.
+  const [conversionAnswer, setConversionAnswer] = useState(null)
   const queryParams = {
     type: 'fiat'
   }
@@ -24,11 +26,31 @@ const Converter = () => {
         console.error(err)
       })
   }, [])
+
+  const handleSubmit = (event) => {
+    // onsubmit calls the passed function (handleSubmit in this case) 
+    event.preventDefault()
+    /*
+    Step 1) Code for handling form data goes here, you should be able to extract the data from the event
+    Remember, the name attribute on the `input` and `select`s are the corresponding names for the data in the form data.
+    */
+
+   /*
+    Step 2) After getting data together, set queryParams based on the currencybeacon docs and use fetch with the appropriate endpoint
+    in order to get the conversion data.
+   */
+
+    /*
+    Step 3) Call setConversionAnswer with the results to set the answer in the component's state.
+    */
+  }
+  
   return (
     <div className="converter-wrapper">
       <h1>Currency Converter</h1>
       <div className="form-wrapper">
-        <form name="converterData">
+      {/* a form's `onsubmit` attribute expects a function  */}
+        <form name="converterData" onsubmit={handleSubmit}>
           <input type="number" name="amount" /> 
           <select name="base">
             {
